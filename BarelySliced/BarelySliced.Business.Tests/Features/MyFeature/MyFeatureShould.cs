ef16648ffc63ace682d88d707236c8e140ef483e
BorisCallens <boriscallens@users.noreply.github.com>
2023-11-24 15:02:34 +0100

    Added bsfeature-business

---
+++ BarelySliced/BarelySliced.Business.Tests/Features/MyFeature/MyFeatureShould.cs
@@ -0,0 +1,23 @@
+﻿using BarelySliced.Business.Features.MyFeature;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BarelySliced.Business.Tests.Features.MyFeature;
+
+public class MyFeatureShould: IClassFixture<UnitTestFixture>
+{
+    public MyFeatureShould(UnitTestFixture fixture)
+    {
+        // fixture.GetRequiredService<>();
+    }
+
+    [Fact]
+    public async Task DoSomething()
+    {
+        var request = new MyFeatureRequest();
+        var handler = new MyFeatureHandler();
+        
+        var response = await handler.Handle(request, CancellationToken.None);
+
+        throw new NotImplementedException("Assert a behaviour of your handler");
+    }
+}
---
+++ BarelySliced/BarelySliced.Business.Tests/UnitTestFixture.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BarelySliced.Business.Tests;
+
+public class UnitTestFixture : IServiceProvider, IDisposable
+{
+    private readonly ServiceProvider _provider;
+
+    public UnitTestFixture()
+    {
+        var serviceCollection = new ServiceCollection();
+        // Add services to the collection
+        _provider = serviceCollection.BuildServiceProvider();
+    }
+
+    public object? GetService(Type serviceType)
+    {
+        return _provider.GetService(serviceType);
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        _provider.Dispose();
+    }
+}
