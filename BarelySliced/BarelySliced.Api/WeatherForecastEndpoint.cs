692ba6b6de18078da600cba86155ea1a7e022c67
BorisCallens <boriscallens@users.noreply.github.com>
2024-04-05 11:06:58 +0200

    Updated nugest + first step towards separating abstraction levels in api

---
+++ BarelySliced/BarelySliced.Api/WeatherForecastEndpoint.cs
@@ -0,0 +1,32 @@
+namespace BarelySliced.Api
+{
+    public static class WeatherForecastEndpoint
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public static void MapWeatherForecastEndpoint(WebApplication app)
+        {
+            app.MapGet("/weatherforecast", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    (
+                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        Random.Shared.Next(-20, 55),
+                        Summaries[Random.Shared.Next(Summaries.Length)]
+                    ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+        }
+        record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+        {
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+}
